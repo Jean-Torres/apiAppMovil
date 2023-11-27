a4807441fe66a680e90e94cdf2aa0eaa6882826b
Jean-Torres <carlos008torres@gmail.com>
2023-11-27 08:06:04 -0500

    Actualizacion de confirm login

--- src/router/SesionUsuarios.routes.js
+++ src/router/SesionUsuarios.routes.js
@@ -4,7 +4,7 @@ import *as usuarioCtrl from '../controllers/SesionUsuarios.controller';
 const router = Router();
 
 router.post('/', usuarioCtrl.createUsuario);
-router.post('/userOne', usuarioCtrl.findOneUsuario);
+router.get('/userOne/:usuario/:contrasenha', usuarioCtrl.findOneUsuario);
 router.get('/userAll', usuarioCtrl.verifyToken, usuarioCtrl.findAllUsuario);
 router.delete('/', usuarioCtrl.verifyToken, usuarioCtrl.deleteUsuario);
 router.put('/', usuarioCtrl.updateUsuario);
